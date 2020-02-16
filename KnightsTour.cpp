cb40ebf6f95035885261b6030ba8c2025473d444
Dylan Frechette <1999djf@gmail.com>
2020-02-15 21:55:18 -0500

    Minor alteration of class, decided to make board entirely internal in class

--- KnightsTour.cpp
+++ KnightsTour.cpp
@@ -11,27 +11,21 @@ Mod History
 #include <stack>
 #include <vector>
 
-/*
-using nested vectors rather than array due to being able to actually alter vector size,
-giving the ability to make a variable the size for our board
-*/
 class Knight
 {
 public:
-    int xStarting;
-    int yStarting;
-    int boardSize;
-    std::vector<std::vector<int> > wholeBoard;
+
     int xMove;
     int yMove;
-    Knight(std::vector<std::vector<int> > Board, int xStart, int yStart, int bSize)
+    Knight(int xStart, int yStart, int bSize)
     {
-        boardSize=bSize;
-        wholeBoard = Board;
-        xStarting=xStart;
-        yStarting=yStart;
+        const int boardSize=bSize;
+        const int xStarting=xStart;
+        const int yStarting=yStart;
+        int wholeBoard[boardSize][boardSize];
     }
-    void KnightMove(int xMove, int yMove, std::vector<std::vector<int> > Board)
+
+    void KnightMove(int xMove, int yMove)
     {
 
     }
@@ -39,7 +33,7 @@ public:
     boolean check if new spots x and y are valid spots for movement
     in board Board of size S
     */
-    bool safeMove(int x, int y, std::vector<std::vector<int> > board, int bSize)
+    bool safeMove(int x, int y, int bSize)
     {
         return (x>=0 && x<bSize && y >=0 && y<bSize)?true:false;
     }
@@ -51,17 +45,13 @@ int main()
     int boardSize;
     int startX;
     int startY;
-    /*using vector of vectors for board due to flexibility*/
-    std::vector<std::vector<int> > board;
-    /*initializing memory for the total size of board*/
-    board.reserve(boardSize*boardSize);
 
-    std::cout << "Welcome to the Knight's Tour" << std::endl;
-    std::cout << "How big is your board?" << std::endl;
+    std::cout << "Welcome to the Knight's Tour\n";
+    std::cout << "How big is your board?\n";
     std::cin >> boardSize;
-    std::cout << "What would you like your knight's starting X coordinate to be?" << std::endl;
+    std::cout << "What would you like your knight's starting X coordinate to be?\n";
     std::cin >> startX;
-    std::cout << "What would you like the starting y coordinate to be?" << std::endl;
+    std::cout << "What would you like the starting y coordinate to be?\n";
     std::cin >> startY;
-    Knight Gallahad(board,startX,startY,boardSize);
+    Knight Gallahad(startX,startY,boardSize);
 }
