f7c0f4e32e0960809af3f7bf455913b3c433c658
Dylan Frechette <1999djf@gmail.com>
2020-02-21 19:11:47 -0500

    Finished for multiple runthroughs using Warnsdorff

--- Knight's Tour/KnightsTour.cpp
+++ Knight's Tour/KnightsTour.cpp
@@ -1,6 +1,6 @@
 /*
 Dylan Frechette
-Knights Tour; Half-Backtracking Half-Warnsdorff's Algorithm
+Knights Tour Using Warnsdorff's Algorithm
 Created: 09/02/20
 */
 
@@ -22,13 +22,15 @@ bool onBoard(int x, int y)
 { 
     return ((x >= 0 && y >= 0) && (x < N && y < N)); 
 } 
-// Checks whether a square is on board and empty
+// Checks whether a square is on board and hasn't been visited
 bool isEmpty(int a[], int x, int y) 
 { 
     return (onBoard(x, y)) && (a[y*N+x] < 0); 
 } 
-/* Returns the number of empty squares adjacent 
-   to (x, y) */
+/* 
+Returns the number of empty squares knight
+is able to move to from (x, y)
+*/
 int getMvDeg(int a[], int x, int y) 
 { 
     int count = 0; 
@@ -44,7 +46,7 @@ returns false if it is not possible to pick next point.
 */
 bool nextMove(int a[], int *x, int *y) 
 { 
-    int min_deg_idx = -1, c, min_deg = (N+1), nx, ny; 
+    int minDegIndex = -1, c, minDegree = (N+1), newx, newy; 
     /*
     Try all N adjacent of (*x, *y) starting 
     from random pick. Find the adjacent 
@@ -54,34 +56,33 @@ bool nextMove(int a[], int *x, int *y)
     for (int count = 0; count < N; ++count) 
     { 
         int i = (start + count)%N; 
-        nx = *x + moveX[i]; 
-        ny = *y + moveY[i]; 
-        if ((isEmpty(a, nx, ny)) && 
-           (c = getMvDeg(a, nx, ny)) < min_deg) 
+        newx = *x + moveX[i]; 
+        newy = *y + moveY[i]; 
+        if ((isEmpty(a, newx, newy)) && 
+           (c = getMvDeg(a, newx, newy)) < minDegree) 
         { 
-            min_deg_idx = i; 
-            min_deg = c; 
+            minDegIndex = i; 
+            minDegree = c; 
         } 
     } 
     //can't find a next point
-    if (min_deg_idx == -1) 
-        //std::cout <<"Couldn't complete path, try again\n";
+    if (minDegIndex == -1) 
         return false; 
   
     // Store next point 
-    nx = *x + moveX[min_deg_idx]; 
-    ny = *y + moveY[min_deg_idx]; 
+    newx = *x + moveX[minDegIndex]; 
+    newy = *y + moveY[minDegIndex]; 
   
     // Mark next move on board
-    a[ny*N + nx] = a[(*y)*N + (*x)]+1; 
+    a[newy*N + newx] = a[(*y)*N + (*x)]+1; 
   
     // Update next point 
-    *x = nx; 
-    *y = ny; 
+    *x = newx; 
+    *y = newy; 
   
     return true; 
 } 
-//display board with all moves
+//display board with all moves and order visited
 void print(int a[]) 
 { 
     for (int i = 0; i < N; ++i) 
@@ -99,26 +100,19 @@ bool neighborSq(int x, int y, int xx, int yy)
         if (((x+moveX[i]) == xx)&&((y + moveY[i]) == yy)) 
             return true; 
   
-    //std::cout <<"Couldn't complete path, try again\n";
     return false; 
 } 
-//makes moves using Warnsdorff, returns false if can't
-bool completeTour() 
+//makes moves using Warnsdorff, returns false if can't complete
+bool completeTour(int sx,int sy) 
 { 
     // Fill board matrix with -1's 
     int a[N*N]; 
     for (int i = 0; i< N*N; ++i) 
         a[i] = -1; 
    
-    int sx = rand()%N; 
-    int sy = rand()%N; 
-    // int sx, sy;
-    // //having issues, sometimes works, sometimes doesn't when
-    // //using input, sometimes just need to input twice
-    // std::cout<<"Pick an x coordinate\n";
-    // std::cin>>sx;
-    // std::cout<<"Pick a y coordinate\n";
-    // std::cin >> sy;
+    // int sx = rand()%N; 
+    // int sy = rand()%N; 
+
     // Current points are same as initial points 
     int x = sx, y = sy; 
     //Mark first location on board
@@ -133,15 +127,111 @@ bool completeTour()
 
     print(a); 
     return true; 
-} 
-
-int main() 
-{ 
-    srand(time(NULL));
-    //Continue until solution found 
-    while (!completeTour()) 
-    { 
-    ; 
-    } 
-    return 0; 
-} 
+}
+//Following struct and function are for linked list generation
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+//intialize head node
+struct Node *head = NULL;
+//insert node to front of linked list
+void insert(int new_data)
+{
+    struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
+    new_node->data = new_data;
+    new_node->next = head;
+    head = new_node;
+}
+//deletes last input node
+  void deleteIn()
+  {
+    Node *temp=new Node;
+    temp=head;
+    head=head->next;
+    delete temp;
+  }
+/*
+displays nodes input from most recent to least,
+displaying every two nodes as a coordinate
+*/
+void display()
+{
+    struct Node *ptr;
+    ptr = head;
+    int count=0;
+    while (ptr != NULL)
+    {
+        if(count%2==0)
+        {
+        std::cout <<"("<< ptr->data << ",";
+        ptr = ptr->next;
+        count++;
+        }
+        else
+        {
+            std::cout << ptr->data << ") ";
+            ptr = ptr->next;
+            count++;
+        }
+        
+    }
+    std::cout << "\n";
+}
+    int main()
+    {
+        //boolean made to check for when finished inputting new points
+        bool doneEdit=false;
+        int x, y, choice;
+        std::cout << "Select an x (0-7)\n";
+        std::cin >> x;
+        std::cout << "select a y(0-7)\n";
+        std::cin >> y;
+        insert(x);
+        insert(y);
+        display();
+        while(!doneEdit)
+        {
+            std::cout << "1. Add new coordinate\n2. Delete last input\n3. continue to tour list\n";
+            std::cin >> choice;
+            if(choice==1)
+            {
+                std::cout << "Select an x(0-7)\n";
+                std::cin >> x;
+                std::cout << "select a y(0-7)\n";
+                std::cin >> y;
+                //insert each separately since values stored as single ints
+                insert(x);
+                insert(y);
+                display();
+                doneEdit=false;
+            }
+            else if(choice==2)
+            {
+                //delete twice due to points being stored as single ints
+                deleteIn();
+                deleteIn();
+                display();
+                doneEdit=false;
+            }
+            else if(choice==3)
+            {
+                doneEdit=true;
+                break;
+            }
+        }
+        while(head != NULL)
+        {
+        //Continue until solution found
+        while (!completeTour(x, y))
+        {
+            ;
+        }
+        //delete the x and y just used in current iteration
+        deleteIn();
+        deleteIn();
+        std::cout <<"\n\nTour complete\n\n";
+        }
+        return 0;
+    }
