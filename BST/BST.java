e310ccd6c3b6ab881ef20de1e0367435bc4a5de3
Dylan Frechette <1999djf@gmail.com>
2020-03-25 22:56:06 -0400

    Finished

--- BST/BST.java
+++ BST/BST.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class BST // binary search tree
+public class BST
 {
     //create TreeNode class
     private class TreeNode
@@ -13,11 +13,6 @@ public class BST // binary search tree
     TreeNode root;
     //create new empty BST
     public BST()
-    {
-        root=null;
-    }
-    //empty BST, shouldn't need
-    public void emptyTree()
     {
         root=null;
     }
@@ -47,32 +42,16 @@ public class BST // binary search tree
             return r;
         }
     }
-    //search for a value in tree, don't think I'll need, but just in case
-    public Comparable search(Comparable x)
-    {
-        return searchHelp(root, x);
-    }
-    //function to assist in search
-    private Comparable searchHelp(TreeNode r, Comparable x)
-    {
-        if(r==null) return null;
-        if(x.compareTo(r.data)==0) return r.data;
-        if(x.compareTo(r.data)<0) return searchHelp(r.left,x);
-        else return searchHelp(r.right,x);
-    }
     //run an inorder traversal to printout values
     public void printInorder(TreeNode x) 
     { 
         if (x == null) 
             return; 
-  
-        /* first recur on left child */
-        printInorder(x.left); 
-  
-        /* then print the data of node */
+        /*recur on left child */
+        printInorder(x.left);   
+        /*print the data of node */
         System.out.print(x.data + "\n"); 
-  
-        /* now recur on right child */
+        /*recur on right child */
         printInorder(x.right); 
     } 
 }
--- BST/BSTMain.java
+++ BST/BSTMain.java
@@ -3,21 +3,22 @@ import java.util.*;
 
 public class BSTMain //extends BST
 {
-    public static void intoHash(HashMap<String, Integer> hm) throws FileNotFoundException, IOException
+    public static void intoHash(HashMap<String, String> hm) throws FileNotFoundException, IOException
     {
         BufferedReader txtRead = new BufferedReader(new FileReader("test.txt"));
         // word you're presently reading
         String currentString = txtRead.readLine();
         // current line, starting at 1
         int currentLine = 1;
-        //is giving line numbers properly, now need to format and push into tree
+        //split line into individual strings, then provide line number as current string
         while (currentString != null) 
         {
             String[] word = currentString.split(" ");
             for (String s : word) 
             {
+                String line=Integer.toString(currentLine);
                 //Integer i = hm.get(s);
-                hm.put(s, currentLine);
+                hm.put(s, line);
             }
             currentString = txtRead.readLine();
             currentLine++;
@@ -27,15 +28,26 @@ public class BSTMain //extends BST
     public static void main(String[] args) throws FileNotFoundException, IOException
     {
         BST BinaryTree=new BST();
-        HashMap<String, Integer> txtFileLine = new HashMap<String, Integer>();
+        HashMap<String, String> txtFileLine = new HashMap<String, String>();
+        //calls intoHash() to insert strings into hashmap
         intoHash(txtFileLine);
-        //having issues here
+        //convert hashmap into an arraylist of strings to insert properly into tree
         List<String> hmList=new ArrayList<String>(txtFileLine.keySet());
         for(String s: hmList)
         {
+            //concatenate value and key to one string
+            s=s+" "+txtFileLine.get(s);
+            //removes punctuation from each string (, . # etc)
+            s=s.replaceAll("\\p{Punct}","");
+            //checks if current string contains alphabetical characters,
+            //if not, passes to next string
+            if(s.matches(".*[a-z].*"))
+            {
             BinaryTree.insert(s);
+            }
+            else continue;
         }
-        BinaryTree.printInorder(BinaryTree.root);
-        //System.out.println(txtFileLine);        
+        //prints all nodes in InOrder form
+        BinaryTree.printInorder(BinaryTree.root);      
     }
 }
