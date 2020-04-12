627f7fc71c61655c64c8ec825ea4c524e8a66b68
Dylan Frechette <1999djf@gmail.com>
2020-04-12 16:44:11 -0400

    Finished algorithms, just need graphs

--- Sorting/SortMain.java
+++ Sorting/SortMain.java
@@ -3,10 +3,72 @@ import java.util.*;
 
 public class SortMain extends Sorts
 {
-
-
+    //random number generator for array values 
+    public static void randomNumbers(Integer[] arr)
+    {
+        for(int i=0;i<arr.length;i++)
+            arr[i]=1+(int)(Math.random()*20000);
+    }
+    // public static void printArray(Integer[] arr) 
+    // {
+    //     if (arr.length <= 100) 
+    //     {
+    //         for (int i = 0; i < arr.length; i++) 
+    //         {
+    //             System.out.println(arr[i]);
+    //         }
+    //     }
+    // }
     public static void main(String [] args)
     {
-        
+        int[] listSizes={100, 500, 1000, 2000, 5000, 8000, 10000};
+        for(int i=0;i<listSizes.length;i++)
+        {
+            //declaring variables to get times of each method
+            int quickStart, quickStop, quickTime, insertionStart, insertionStop
+            ,insertionTime, mergeStart, mergeStop, mergeTime, heapStart, heapStop, heapTime;
+            quickStart=quickStop=quickTime=insertionStart=insertionStop=insertionTime=
+            mergeStart=mergeStop=mergeTime=heapStart=heapStop=heapTime=0;
+            int arrLen=listSizes[i];
+            Integer[] sortingArr= new Integer[arrLen];
+            randomNumbers(sortingArr);
+            //create a copy array to reset unsorted after each sort
+            Integer[] storedArr = new Integer[arrLen];
+            for (int itr = 0; itr < storedArr.length; itr++) 
+                storedArr[itr] = sortingArr[itr];
+            //run quicksort and record time
+            quickStart=(int)System.currentTimeMillis();
+            quick(sortingArr);
+            quickStop=(int)System.currentTimeMillis();
+            quickTime=quickStop-quickStart;
+            System.out.println("Time for "+ listSizes[i]+" values for quicksort: "+quickTime);
+            //reset sorted array
+            for (int itr = 0; itr < storedArr.length; itr++)
+                sortingArr[itr] = storedArr[itr];
+            //run insertion sort and record time
+            insertionStart=(int)System.currentTimeMillis();
+            insertion(sortingArr);
+            insertionStop=(int)System.currentTimeMillis();
+            insertionTime=insertionStop-insertionStart;
+            System.out.println("Time for " + listSizes[i] + " values for insertion sort: " + insertionTime);
+            // reset sorted array
+            for (int itr = 0; itr < storedArr.length; itr++)
+                sortingArr[itr] = storedArr[itr];
+            //run heapsort and record time
+            heapStart=(int)System.currentTimeMillis();
+            heapSort(sortingArr);
+            heapStop=(int)System.currentTimeMillis();
+            heapTime=heapStop-heapStart;
+            System.out.println("Time for " + listSizes[i] + " values for heapsort: " + heapTime);
+            // reset sorted array
+            for (int itr = 0; itr < storedArr.length; itr++)
+                sortingArr[itr] = storedArr[itr];
+            //run mergesort and record time
+            mergeStart=(int)System.currentTimeMillis();
+            merge(sortingArr);
+            mergeStop=(int)System.currentTimeMillis();
+            mergeTime=mergeStop-mergeStart;
+            System.out.println("Time for " + listSizes[i] + " values for mergesort: " + mergeTime);
+        }
     }
 }
--- Sorting/Sorts.java
+++ Sorting/Sorts.java
@@ -4,6 +4,106 @@ import java.math.*;
 
 public class Sorts
 {
+    //general swap method for use in sorts
+    private static void swap(Object[] list, int x, int y)
+    {
+        Object tmp = list[x];
+        list[x] = list[y];
+        list[y] = tmp;
+    }
+    //method for insertion sort
+    public static void insertion(Comparable[] list) 
+    {
+        for (int i = 1; i < list.length; i++) 
+        {
+            Comparable itemtoinsert = list[i];
+            int j;
+            for (j = i - 1; j >= 0 &&
+            list[j].compareTo(itemtoinsert) > 0; j--) 
+            {
+                list[j + 1] = list[j];
+            }
+            list[j + 1] = itemtoinsert;
+        }
+    }
+    //methods for quicksort
+    public static void quick(Comparable[] list) 
+    {
+        quickhelp(list, 0, list.length - 1);
+    }
+
+    private static void quickhelp(Comparable[] list, int start, int stop) {
+        // base cases
+        if (stop <= start)
+            return; // size is 0 or 1
+        if (start + 1 == stop) // size 2
+        {
+            if (list[start].compareTo(list[stop]) > 0)
+                swap(list, start, stop);
+            return;
+        }
+        // recursive case
+        int pivotpos = partition(list, start, stop);
+        quickhelp(list, start, pivotpos - 1);
+        quickhelp(list, pivotpos + 1, stop);
+    }
+
+    private static int partition(Comparable[] list, int start, int stop) 
+    {
+        Comparable pivot = list[stop];
+        int fr = start;
+        for (int i = start; i < stop; i++) 
+        {
+            if (list[i].compareTo(pivot) < 0) 
+            {
+                swap(list, i, fr);
+                fr++;
+            }
+        }
+        swap(list, fr, stop);
+        return fr;
+    }
+    //methods for heapsort
+    public static void heapSort(Integer[] arr) 
+    {
+        int arrayLen = arr.length;
+        // Build heap
+        for (int i = arrayLen / 2 - 1; i >= 0; i--)
+            heapify(arr, arrayLen, i);
+        // One by one extract elements from heap
+        for (int i = arrayLen - 1; i > 0; i--) 
+        {
+            // Move current root to end of array
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            // call max heapify on the reduced heap
+            heapify(arr, i, 0);
+        }
+    }
+
+    private static void heapify(Integer[] arr, int n, int root) 
+    {
+        //set largest to root
+        int greatest = root;
+        int left = 2 * root + 1;
+        int right = 2 * root + 2;
+        // If left is larger than root
+        if (left < n && arr[left] > arr[greatest])
+            greatest = left;
+        // If right is larger than current greatest
+        if (right < n && arr[right] > arr[greatest])
+            greatest = right;
+        // If largest is not root
+        if (greatest != root) {
+            int swap = arr[root];
+            arr[root] = arr[greatest];
+            arr[greatest] = swap;
+            // Recursively heapify sub-tree
+            heapify(arr, n, greatest);
+        }
+    }
+    //methods for mergesort
     public static void merge(Comparable [] list)
 	{
 		mergehelp(list,0,list.length-1);
